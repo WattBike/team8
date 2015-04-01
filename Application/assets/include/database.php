f54b95a775c903431e135a86c42c092b67e324cc
charlottehanekamp <charlotte.hanekamp@hva.nl>
2015-04-01 12:13:58 +0200

    nieuwe map
    nieuwe map met daarin de bestanden voor de application

---
+++ Application/assets/include/database.php
@@ -0,0 +1,7 @@
+<?php
+date_default_timezone_set('Europe/Amsterdam');
+$mysqliUsername = "hanekac001";
+$mysqliPassword = "CH\$n3k4mp";
+$mysqliHost = "localhost"; 
+$mysqliDatabase="zhanekac001";
+?>
