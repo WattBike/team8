12c59e6a5028b4ee1a0d1c18050a55ee5ee08668
Sean Molenaar <sean.molenaar@hva.nl>
2015-04-01 11:58:52 +0200

    Remove db info from the interwebs

--- assets/include/database.php
+++
@@ -1,7 +0,0 @@
-<?php
-date_default_timezone_set('Europe/Amsterdam');
-$mysqliUsername = "hanekac001";
-$mysqliPassword = "CH\$n3k4mp";
-$mysqliHost = "localhost"; 
-$mysqliDatabase="zhanekac001";
-?>
