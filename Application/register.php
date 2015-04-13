cd3f8740bbdf10e9411c9f16a42bf5dff79bad5c
Sean Molenaar <sean.molenaar@hva.nl>
2015-04-13 12:14:20 +0200

    Make a registration page

--- Application/register.php
+++ Application/register.php
@@ -6,12 +6,11 @@ require_once(__ROOT__ . '/assets/include/connect.php');
 require_once(__ROOT__ . '/assets/include/header.php');
 if (isset($_POST['email']) && ($_POST['email'] != "")):
     if (!verified_login($_POST['email'], $_POST['password'])) {
-        echo "Login Failed";
-        //header('Location: /index.php', 401);
+        echo "Registration Failed";
     } else {
-        echo "Login Succeeded!";
-        //header('Location: /index.php', 200);
+        echo "Registration Succeeded!";
     }
+header('Location: /index.php');
 else:
 ?>
 <div class="well card">
@@ -20,19 +19,8 @@ else:
     <span id="reauth-email" class="reauth-email"></span>
     <input type="email" id="inputEmail" name="email" class="form-control" placeholder="Email address" required autofocus>
     <input type="password" id="inputPassword" name="password" class="form-control" placeholder="Password" required>
-    <div id="remember" class="checkbox">
-      <label>
-        <input type="checkbox" value="remember-me"> Remember me
-      </label>
-    </div>
-    <button class="btn btn-primary btn-lg btn-block" type="submit">Sign in</button>
-    <a href="register.php" class="btn btn-default btn-lg btn-block pull-right">
-    Sign up
-    </a>
+    <button class="btn btn-primary btn-lg btn-block" type="submit">Register</button>
   </form><!-- /form -->
-  <a href="#" class="forgot-password">
-    Forgot the password?
-  </a>
 </div><!-- /card-container -->
 <?php
 endif;
