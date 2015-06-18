f3f747da0dd2d52b05ac5aba930f85b054f872ff
charlottehanekamp <charlotte.hanekamp@hva.nl>
2015-06-18 09:53:53 +0200

    Design + PHP

---
+++ Design/include/config.php
@@ -0,0 +1,15 @@
+<?php
+if(!defined('safeGuard')) {
+   die('Direct access not permitted');
+}
+
+date_default_timezone_set('Europe/Amsterdam');
+$mysqliUsername = "molenas005";
+$mysqliPassword = "n0MFRXoJmKn1PK";
+$mysqliHost = "oege.ie.hva.nl"; 
+$mysqliDatabase="zmolenas005";
+
+$base_url="/test";
+
+$passwordPreSalt="k4wQ|lh|jh~^ztYxy|HFHp_AEkYxXoDjoCu3pLF5wNred-yS0ggSrqf^9NC";
+?>
